ef0be7f41b21e92caa7c91af6660841b84c1ccd2
vinodkumar0705 <vinodkumarsram@gmail.com>
2025-06-11 10:37:28 +0530

    At server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'appointment.html'));
 });
 
-// Start server
+// Start server forbackend
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
