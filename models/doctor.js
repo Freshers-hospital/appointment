0c87dcf869daacc260986a03581d2eda2896f5b8
hospital-project-freshers <subhadraashu@gmail.com>
2025-06-17 18:44:46 +0530

    june 17 made changes in admindashboard

--- models/doctor.js
+++ models/doctor.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const doctorSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  specialty: String,
+  specialty: { type: String, required: true },
+  experience: { type: String },
+  education: { type: String },
+  image: { type: String }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Doctor', doctorSchema);
