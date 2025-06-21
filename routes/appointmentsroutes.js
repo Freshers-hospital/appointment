c65254cec92d546afdfee52543ad63f1c61018df
hospital-project-freshers <subhadraashu@gmail.com>
2025-06-21 10:19:27 +0530

    june 21

--- routes/appointmentsroutes.js
+++ routes/appointmentsroutes.js
@@ -4,29 +4,4 @@ const { getTodayCounts } = require('../controllers/appointments');
 
 router.get('/today-count', getTodayCounts);
 
-
-
-// Add this POST route below
-router.post('/appointments', async (req, res) => {
-  try {
-    const { doctorId, patientId, date } = req.body;
-
-    const appointment = new Confirmation({
-      doctorId,
-      patientId,
-      date: new Date(date),
-    });
-
-    await appointment.save();
-
-    res.status(201).json({ message: 'Appointment booked successfully' });
-  } catch (error) {
-    console.error('Error booking appointment:', error);
-    res.status(500).json({ error: 'Server error while booking appointment' });
-  }
-});
-
-
-
-
 module.exports = router;
