c854d0c6b45c124412e6c70bf9744b9f7f37f715
teamaccentiqa <teamaccentiqa@gmail.com>
2025-07-31 14:55:52 +0530

    fixed profile name display issue, removed doctor icons and added default icon.

--- models/doctor.js
+++ models/doctor.js
@@ -19,7 +19,8 @@ const doctorSchema = new mongoose.Schema({
   age: { type: Number },
   gender: { type: String },
   phone: { type: String },
-  address: { type: String }
+  address: { type: String },
+  isDeleted: { type: Boolean, default: false },
 }, { timestamps: true });
 
 doctorSchema.index({ firstName: 1, lastName: 1 }, { unique: true });
